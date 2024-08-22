0afd35313b2d53c4e2ffe742c8097be2a80cd2e6
reshmamolm <reshmamolm2001@gmail.com>
2024-08-22 12:57:31 +0530

    counterapplication

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// to initialize the file as slice we use a method createSlice()
+export const counterSlice=createSlice({
+    // name has no importance
+    name:"counterApp",
+
+    // initial state creation
+    initialState:{
+        value:0
+    },
+    reducers:{
+        // logic to update the value
+        // action are created inside the reducers
+        increment:(state,data)=>{
+            if(!data.payload){
+                state.value= state.value +1
+            }
+            else{
+                state.value = state.value+ data.payload
+            }
+        },
+        decrement:(state,data)=>{
+            if(!data.payload){
+                state.value= state.value -1
+            }
+            else{
+                state.value=state.value - data.payload // state.value-=1 (shorthand operator)
+            } 
+        },
+        reset:(state)=>{
+            state.value=0;
+        }
+    }
+
+});
+export const {increment,decrement,reset}=counterSlice.actions;
+export default counterSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
+
+//we have to use a method configureStore to initialize this file as store
+ export const store = configureStore({
+    reducer:{
+        // to add reducers
+        counter:counterSlice
+    }
+
+})
